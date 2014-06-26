86666f3fd6fd52fbad6064a598a88a6bc905fd59
Philip Woods <elzairthesorcerer@gmail.com>
2014-06-26 15:26:57 -0400

    Got SHA1 working for one-block messages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs')
   , sha1 = require('./lib/sha1')
   ;
 
-exports.sha1 = function(str) {
+exports.sha1 = function(str, test_data_path) {
   var strbuf = new Buffer(str, 'utf8');
   var fsize = strbuf.length;
   
@@ -14,7 +14,14 @@ exports.sha1 = function(str) {
 
   var post_results = sha1.postread_sha1(buff, results, fsize);
 
-  var out = sha1.sha1(post_results.buff);
+  var out = null;
+  if (test_data_path) {
+    var test_data = JSON.parse(fs.readFileSync(test_data_path));
+    out = sha1.sha1(post_results.buff, test_data);
+  }
+  else {
+    out = sha1.sha1(post_results.buff);
+  }
 
   return out;
 };
