ee4bc6e84f92c36ea839104259c06afd1155faba
Philip Woods <elzairthesorcerer@gmail.com>
2014-06-26 11:28:07 -0400

    Renamed libs/ to lib/ and removed extraneous comments from index.js

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 var fs = require('fs')
-  , sha1 = require('./libs/sha1')
+  , sha1 = require('./lib/sha1')
   ;
 
 exports.sha1 = function(str) {
-  // Allocate temporary buffer containing str
   var strbuf = new Buffer(str, 'utf8');
-
-  // Get size of string buffer  in bytes
   var fsize = strbuf.length;
   
-  // Calculate needed size of string buffer
   var results = sha1.preread_sha1(fsize);
 
-  // Initialize string buffer to size of input in bytes
   var buff = new Buffer(results.hsize);
   buff.fill(0);
-
-  // Write string to string buffer
   strbuf.copy(buff);
 
-  // Pad buffer & append original buffer size
   var post_results = sha1.postread_sha1(buff, results, fsize);
 
-  // Generate SHA-1 hash of buffer
   var out = sha1.sha1(post_results.buff);
 
   return out;
